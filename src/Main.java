f1a53f588e5237b5ef4f8ded7cf2564eaf5378f2
LoveyouMaryme <love-mary.victor@outlook.com>
2026-02-21 13:27:12 -0500

    adding algo to separate uppercases

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,21 @@ void main() {
 
     Tengwars test = new Tengwars("LAURIE LAntAR LASSI SURINEN");
 
-    test.parcourirChaineCaractere("LAURIE LAntAR LASSI SURINEN");
 
     TengwarScript T =  TengwarScript.TINCO;
 
-    System.out.println(TengwarScript.values().length);
+    //System.out.println(TengwarScript.values().length);
 
-    test.translateCharacters("T");
+    //System.out.println(Tengwars.translateLatinLetter("T"));
+
+    //test.breakdownSentences("LAURIE LAntAR LASSI SURINEN");
+
+    // System.out.println(test.getLatinSequence("LAURIE"));
+    // System.out.println(test.getLatinSequence("LAntAR"));
+
+    System.out.println(test.separateUpperCase("LAntAR"));
+
+
 
 
 
--- src/Tengwars.java
+++ src/Tengwars.java
@@ -1,3 +1,5 @@
+import java.lang.classfile.ClassTransform;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Tengwars {
@@ -11,39 +13,134 @@ public class Tengwars {
         this.phrase = phrase;
     }
 
-    public String translateCharacters(String latinLetter){
+//    public String translateCharacters(String latinLetter){
+//
+//        TengwarScript[] TengwarLetters = TengwarScript.values();
+//        for(int i = 0; i < TengwarLetters.length; i++){
+//
+//            System.out.println("Day at index " + i + ": " + TengwarLetters[i]);
+//
+//            TengwarScript test = new TengwarScript(TengwarLetters[i]);
+//
+//            if(latinLetter.equals(TengwarLetters[i])){
+//
+//                System.out.println("yay");
+//
+//            }
+//
+//
+//
+//        }
+//
+//        return "";
+//
+//
+//    }
 
-        TengwarScript[] TengwarLetters = TengwarScript.values();
-        for(int i = 0; i < TengwarLetters.length; i++){
+    public static String translateLatinLetter(String latinLetters){
 
-            System.out.println("Day at index " + i + ": " + TengwarLetters[i]);
+        TengwarScript [] alphabeticalTengar =  TengwarScript.values();
+        String translatedLetter = "";
 
-            TengwarScript test = new TengwarScript(TengwarLetters[i]);
+        for(int i = 0; i < alphabeticalTengar.length ; i++){
 
-            if(latinLetter.equals(TengwarLetters[i])){
+            if(latinLetters.equals(alphabeticalTengar[i].getClassic())){
 
-                System.out.println("yay");
-
+                translatedLetter = alphabeticalTengar[i].getCommandeLaTeX();
             }
-
-
 
         }
 
-        return "";
-
-
+        return translatedLetter;
     }
-    public String parcourirChaineCaractere(String phrase){
 
-         Scanner sc = new Scanner(phrase).useDelimiter(".");
 
-         while(sc.hasNext()){
-             System.out.println(sc.next());
-         }
+    public boolean isCapitalizedLetter(char letter){
+        boolean isCapitalized = true;
+        int zCharNumber = 90;
 
-         sc.close();
+        if(letter >=  zCharNumber){
+            isCapitalized = false;
+        }
 
-        return "";
+
+        return isCapitalized;
     }
+
+    public String[] breakdownSentences(String phrase){
+
+        String[] words = phrase.split(" ");
+
+        return words;
+    }
+
+    public ArrayList<Character> getLatinSequenceUpperCase(String word) {
+
+        ArrayList<Character> latinSequenceUpperCase = new ArrayList<>();
+        char[] array_sentence = word.toCharArray();
+        int wordLength = array_sentence.length;
+        char letter = array_sentence[wordLength - 1];
+
+        if (isCapitalizedLetter(letter)) {
+            latinSequenceUpperCase.add(letter);
+
+        }
+        return latinSequenceUpperCase;
+    }
+
+    public ArrayList<String> separateUpperCase(String word) {
+
+        ArrayList<String> separatedUpperCase = new ArrayList<>();
+        char[] array_sentence = word.toCharArray();
+        int wordLength = array_sentence.length;
+
+        for(int i = wordLength - 1; i >= 0; i--){
+
+            char letter = array_sentence[i];
+
+            if (isCapitalizedLetter(letter)) {
+            separatedUpperCase.addFirst(String.valueOf(letter));
+            } else if( letter == 'u' || letter == 'w'){
+
+                String lowerCase = "" + array_sentence[i-2] + array_sentence[i-1] + letter;
+                separatedUpperCase.addFirst(lowerCase);
+
+                i--;
+                i--;
+            } else {
+                String lowerCase = ""   + array_sentence[i-1] + letter ;
+                separatedUpperCase.addFirst(lowerCase);
+                i--;
+            }
+
+        }
+
+//
+//
+//        if (letter == 'u' || letter == 'w'){
+//        }
+        return separatedUpperCase;
+    }
+
+    public ArrayList<Character> getLatinSequenceLowerCase(String word){
+
+        ArrayList<Character> latinSequenceLowerCase = new ArrayList<>();
+
+        char[] array_sentence = word.toCharArray();
+        int wordLength = array_sentence.length;
+
+        for (int i = wordLength - 1; i >= 0; i--) {
+
+            char letter = array_sentence[i];
+
+            if (!isCapitalizedLetter(letter)) {
+                latinSequenceLowerCase.add(letter);
+            }
+
+
+        }
+        return latinSequenceLowerCase;
+    }
+
+
 }
