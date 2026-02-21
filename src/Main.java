60e4f92b136b1c44398b86f318128606c904304d
Sami Mouzai <ea391026@ens.uqam.ca>
2026-02-21 17:48:58 -0500

    ajout d'un scanner dans main pour formater une phrase

--- src/Main.java
+++ src/Main.java
@@ -15,15 +15,24 @@ void main() {
 
     // System.out.println(test.getLatinSequence("LAURIE"));
     // System.out.println(test.getLatinSequence("LAntAR"));
-    System.out.println(test.separateUpperCase("LAntARuuqungwAnquV"));
+    //System.out.println(test.separateUpperCase("LAntARuuqungwAnquV"));
 
     //System.out.println(test.separateUpperCase("LAntARuuqungwAnqu"));
-    System.out.println(Tengwars.quenyaVersTengwarLaTeX("LAURIE LAntAR LASSI SURINEN"));
+    //System.out.println(Tengwars.quenyaVersTengwarLaTeX("LAURIE LAntAR LASSI SURINEN"));
+
+    String phraseTengwar = Tengwars.quenyaVersTengwarLaTeX("LAURIE LAntAR LASSI SURINEN");
+    Scanner scanner = new Scanner(phraseTengwar);
+
+    while (scanner.hasNext()){
+        System.out.println(scanner.next());
+    }
+
+    scanner.close();
     //System.out.println(Tengwars.quenyaVersTengwarLaTeX("AndUNE PELLA"));
 
     //System.out.println(test.parcourirStringToTranslate(test.separateUpperCase("AndUNE")));
 
-    test.translateLatinLetter("V");
+    //test.translateLatinLetter("V");
 
 
 
